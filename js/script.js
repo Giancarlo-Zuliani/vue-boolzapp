4c763628ce150c943cc0d0f288f17dd3a5d9b3d7
ginancrlo <slaigox@gmail.com>
2020-11-23 16:00:45 +0100

    milestone 2 complete

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const APP = new Vue({
   el:'#app',
   data: {
-    chatindex:0,
-    emotiHidden : true,
+    chatindex: 0 ,
+    emotiHidden : true ,
     emoticonArray:['😀','😾', '😃', '😄', '😆', '😅', '😂', '🤣','😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😋', '😛', '😝', '😜', '😎', '🤩', '🥳', '😏', '😒', '😞', '😔','😖', '😫', '😩', '🥺','😢','😭','😤','🤬','🤯','😳','🥵','🥶','😱','😨','😰','😥','😓','🤗','😐','😬','🙄','😯','😦','😮','😲','🥱', '😴','😪', '😵', '🤐', '🤢', '🤮', '🤧', '😷', '🤒' ,'🤕' ,'🤑' ,'🤠' ,'😈' ,'👹' ,'👺' ,'🤡' ,'💩' ,'👻', '💀', '👽' , '🤖' ,'🎃','😺'],
     textarea : '',
-    contact :[{
+    contacts :[{
       name:'Michele',
       lastOnline :'10/01/2020 16:15:55',
       img : 'assets/avatar_1.jpg',
-      message :[
+      messages :[
         {text:'vieni  bere una birra?',status: 'received',datainfo:'10/01/2020 16:14:50'},
         {text:'non posso la morosa mi rompe le palle',status:'sent',datainfo:'10/01/2020 16:15:00'}
       ]
@@ -18,7 +18,7 @@ const APP = new Vue({
       name:'Fabio',
       lastOnline :'20/05/2020 20:20:55',
       img:'assets/avatar_2.jpg',
-      message :[
+      messages :[
         {text:'chi sei?!',status:'received',datainfo:'20/05/2020 20:18:50'},
         {text:'no chi sei tu',status:'sent',datainfo:'20/05/2020 20:19:00'},
         {text:'vaffanculo!',status:'received',datainfo:'20/05/2020 20:19:35'},
@@ -29,7 +29,7 @@ const APP = new Vue({
       name:'Abdul',
       lastOnline:'25/12/2019 18:55:59',
       img:'assets/avatar_3.jpg',
-      message:[
+      messages:[
         {text:'ciao abdul ci sei per un cd verde veloce?',status:'sent',datainfo:'25/12/2019 18:50:00'},
         {text:'ciao amico 20 euro',status:'received',datainfo:'25/12/2019 18:51:00'},
         {text:'ascolta ne ho solo dieci perche la  tirchia di mia nonna non mi ha lasciato la mancia per natale',status:'sent',datainfo:'18:51:03'},
@@ -44,7 +44,7 @@ const APP = new Vue({
       name:'Luisa',
       lastOnline:'23/11/2020 15:00:55',
       img:'assets/avatar_4.jpg',
-      message:[
+      messages:[
         {text:'ciao',status:'received',datainfo:'20/11/2019 15:00:02'},
         {text:'non so chi sei ma mi piacciono molto i tuoi baffi',status:'sent',datainfo:'20/11/2019 15:55:00'}
       ]
